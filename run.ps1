0c8d51c64d1ebeddf85af12ccb67be63efd3e8f2
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2021-09-30 10:33:56 -0400

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,10 @@
+$RawPC=''
+$ComputerList=''
+
+
+
 #Getting list of computers in AD that have any Windows 7 edition OS installed
-$computers1 = get-adcomputer -filter {OperatingSystem -like 'Windows 7 *'} -properties Name -SearchBase 'ou=<YOUR OU HERE>,DC=<YOUR DOMAIN HERE>,DC=<YOUR DOMAIN HERE>'
+$RawPC = get-adcomputer -filter {OperatingSystem -like 'Windows 7 *'} -properties Name -SearchBase 'ou=CHANGEME,DC=CHANGEME,DC=CHANGEME'
 
 #Function to grab logged in user if the computer is validates as being online
 function Get-LoggedOnUser {
@@ -17,26 +22,30 @@ function Get-LoggedOnUser {
    }
  }
 
-#Gather data for each computer in $computers1
-foreach ($computer in $computers1) { 
-  $pcinfo = Get-ADComputer $computer.Name -Properties lastlogontimestamp | ` 
-       Select-Object @{Name="Computer";Expression={$_.Name}}, ` 
-       @{Name="Lastlogon";Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}}
 
-  #Execute Get-LoggedOnUser function for current computer
-  $username = Get-LoggedOnUser -ComputerName $computer.Name | Select-Object username
 
-  #If the computer was offline and the $username variable is blank, see if there is an SID identifier from the lastlogon property and notate
-  $SecIdentifier = New-Object System.Security.Principal.SecurityIdentifier($lastuserlogoninfo.SID)
-  if (!$username) { $username = $SecIdentifier.Translate([System.Security.Principal.NTAccount]) }
 
-  #Creating object property table with gathered info
-  $properties = @{'Computer'=$pcinfo.Computer;
-          'LastLogon'=$pcinfo.Lastlogon;
-          'User'=$username.value
-          } #end $properties
+foreach ($comp in $RawPC) {
+    if ($comp.Enabled) {
+        
+        #Gather data for each computer that's enabled
+        $pcinfo = Get-ADComputer $comp.Name -Properties lastlogontimestamp | Select-Object @{Name="Computer";Expression={$_.Name}}, @{Name="Lastlogon";Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}}      
+        
+        #Execute Get-LoggedOnUser function for current computer
+        $username = Get-LoggedOnUser -ComputerName $comp.Name | Select-Object username
 
-  #Append info to win7.csv file in current directory
-  $out = write-output (New-Object -Typename PSObject -Property $properties)
-  add-content -Path win7.txt -Value $out 
-};
+        #If the computer was offline and the $username variable is blank, see if there is an SID identifier from the lastlogon property and notate
+        $lastuserlogoninfo = Get-WmiObject -Class Win32_UserProfile -ComputerName $comp.name | Select-Object -First 1
+        if ($lastuserlogoninfo.SID) {$SecIdentifier = New-Object System.Security.Principal.SecurityIdentifier($lastuserlogoninfo.SID)}
+        if (!$username) { $username = $SecIdentifier.Translate([System.Security.Principal.NTAccount]) }
+
+        $properties = @{
+            'Computer'=$pcinfo.Computer;
+            'LastLogon'=$pcinfo.Lastlogon;
+            'User'=$username.value
+        }
+        $out = write-output (New-Object -Typename PSObject -Property $properties)
+        add-content -Path win7.txt -Value $out 
+    }
+}
+
