586715d6fa8023e3cc69dbc392eb9440a642bc12
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2021-10-17 19:03:10 -0400

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -37,19 +37,8 @@ foreach ($comp in $RawPC) {
         #Execute Get-LoggedOnUser function for current computer
         $username = Get-LoggedOnUser($comp.Name) | Select-Object username
 
-        #CURRENTLY BROKEN - If the computer was offline and the $username variable is blank, see if there is an SID identifier from the lastlogon property and notate
-        #if ($username) {
-        #  try { $lastuserlogoninfo = Get-WmiObject -Class Win32_UserProfile -ComputerName $comp.name | Select-Object -First 1 }
-        #  catch [System.UnauthorizedAccessException] {}
-        #  catch [System.Management.ManagementException] {}
-        #  catch [System.Exception] {}
-        #}
-        #if ($lastuserlogoninfo.SID) {$SecIdentifier = New-Object System.Security.Principal.SecurityIdentifier($lastuserlogoninfo.SID)}
-        #if (!$username) { $username = $SecIdentifier.Translate([System.Security.Principal.NTAccount]) }
-
-
-        if ($username) { $username = "<NoUser>" }
-
+        if ($username) { $username = "n/a" }
+        
         $out = write-output ($pcinfo.Computer + "," + $pcinfo.Lastlogon + "," + $username.value)
         write-output($out)
         add-content -Path win7.csv -Value $out 
