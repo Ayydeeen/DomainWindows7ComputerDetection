718093eabe23ab0adc5830693d66821e0c708c81
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2021-10-17 16:13:45 -0400

    Update run.ps1
    Running into issues where win32_computersystem wmiobject class isn't returning 'Username' or 'SID' values. Researched alternative - none available w/ current security policies.. Attempt to implement error handling - successful for get-loggedonuser function. Working through SID grab/translate due to RPC errors that are posting.

--- run.ps1
+++ run.ps1
@@ -1,46 +1,57 @@
+#Variable Declaration
+
 $RawPC=''
 $ComputerList=''
 
-
-
-#Getting list of computers in AD that have any Windows 7 edition OS installed
-$RawPC = get-adcomputer -filter {OperatingSystem -like 'Windows 7 *'} -properties Name -SearchBase 'ou=CHANGEME,DC=CHANGEME,DC=CHANGEME'
+-------------------------
+#Functions
 
 #Function to grab logged in user if the computer is validates as being online
-function Get-LoggedOnUser {
-   [CmdletBinding()]
-   param (
-     [Parameter()]
-     [ValidateScript({ Test-Connection -ComputerName $_ -Quiet -Count 3 })]
-     [ValidateNotNullOrEmpty()]
-     [string[]]$ComputerName = $env:COMPUTERNAME
-   )
-   foreach ($comp in $ComputerName) {
-     $output = @{ 'ComputerName' = $comp }
-     $output.UserName = (Get-WmiObject -Class win32_computersystem -ComputerName $comp).UserName
-     [PSCustomObject]$output
+function Get-LoggedOnUser($name) {
+   if (Test-Connection -ComputerName $name -Quiet -Count 1) {
+      $output = @{ 'ComputerName' = $Name }
+      $output.UserName = (Get-WmiObject -Class win32_computersystem -ComputerName $name).UserName
+      [PSCustomObject]$output
+   }
+   else {
+      write-output($name + " is offline.")
    }
  }
 
+------------------------
+#Main
 
+#Getting list of computers in AD that have any Windows 7 edition OS installed
+$RawPC = get-adcomputer -filter {OperatingSystem -like 'Windows 7 *'} -properties Name -SearchBase 'ou=MHCOMPUTERS,DC=MHCLINICAL,DC=local'
 
+#Add headers to .csv file
+add-content -Path win7.csv -Value "Computer Name , Last Logon , Current User (if found)"
 
+#For every Windows 7 computer detected in the get-adcomputer command
 foreach ($comp in $RawPC) {
-    if ($comp.Enabled) {
-        
+   #Check to see if computer is Enabled on the company domain
+   if ($comp.Enabled) {
         #Gather data for each computer that's enabled
         $pcinfo = Get-ADComputer $comp.Name -Properties lastlogontimestamp | Select-Object @{Name="Computer";Expression={$_.Name}}, @{Name="Lastlogon";Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}}      
         
         #Execute Get-LoggedOnUser function for current computer
-        $username = Get-LoggedOnUser -ComputerName $comp.Name | Select-Object username
+        $username = Get-LoggedOnUser($comp.Name) | Select-Object username
 
-        #If the computer was offline and the $username variable is blank, see if there is an SID identifier from the lastlogon property and notate
-        $lastuserlogoninfo = Get-WmiObject -Class Win32_UserProfile -ComputerName $comp.name | Select-Object -First 1
-        if ($lastuserlogoninfo.SID) {$SecIdentifier = New-Object System.Security.Principal.SecurityIdentifier($lastuserlogoninfo.SID)}
-        if (!$username) { $username = $SecIdentifier.Translate([System.Security.Principal.NTAccount]) }
+        #CURRENTLY BROKEN - If the computer was offline and the $username variable is blank, see if there is an SID identifier from the lastlogon property and notate
+        #if ($username) {
+        #  try { $lastuserlogoninfo = Get-WmiObject -Class Win32_UserProfile -ComputerName $comp.name | Select-Object -First 1 }
+        #  catch [System.UnauthorizedAccessException] {}
+        #  catch [System.Management.ManagementException] {}
+        #  catch [System.Exception] {}
+        #}
+        #if ($lastuserlogoninfo.SID) {$SecIdentifier = New-Object System.Security.Principal.SecurityIdentifier($lastuserlogoninfo.SID)}
+        #if (!$username) { $username = $SecIdentifier.Translate([System.Security.Principal.NTAccount]) }
 
+
+        if ($username) { $username = "<NoUser>" }
 
         $out = write-output ($pcinfo.Computer + "," + $pcinfo.Lastlogon + "," + $username.value)
+        write-output($out)
         add-content -Path win7.csv -Value $out 
     }
 }
